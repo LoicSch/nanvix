53cfb4ebcb555a4b8dbcfa737942891f5e3cf1dd
LoicSo <souchon.loic@free.fr>
2019-03-20 11:52:57 +0100

    correction increment counter

--- src/kernel/mm/paging.c
+++ src/kernel/mm/paging.c
@@ -353,8 +353,8 @@ PUBLIC void increase_counter() {
 	for (int i = 0; i < NR_FRAMES; i++) {
 		
 		pg = getpte(curr_proc, frames[i].addr);
-		frames[i].counter = frames[i].count >> 1;
-		frames[i].counter = pg->accessed << (sizeof(int)*8-1) | frames[i].counter;
+		frames[i].counter >>= 1;
+		frames[i].counter |= pg->accessed << (sizeof(int)*8-1);
 		
 	}
 }
